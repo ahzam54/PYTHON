b0ee70f6660b5a528ad01f6844d157635ca29bde
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-05-18 17:19:19 +0000

    Add Grade.py, Grade01.py, and Grade02.py for conditional grading exercises

---
+++ Code_Practices/Conditionals/Grade01.py
@@ -0,0 +1,14 @@
+# Using conditionals to determine letter grades based on a numeric score
+# AND operator
+score = int(input("Score: "))
+
+if   90 <= score < 100:
+    print("Grade: A")
+elif   80 <= score < 90:
+    print("Grade: B")
+elif  70 <=score <80:
+    print("Grade: c")
+elif   60 <= score <70:
+    print("Grade: D")
+else:
+    print("Grade: F")
---
+++ Code_Practices/Conditionals/parity.py
@@ -0,0 +1,7 @@
+
+x = int(input("Enter a number: "))
+
+if x % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
