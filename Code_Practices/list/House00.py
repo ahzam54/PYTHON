5c72e390514e94470fbfc855e70a829be285ebfc
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-05-22 13:53:27 +0000

    Add House00.py to create a list of students and print each student's name

---
+++ Code_Practices/list/House00.py
@@ -0,0 +1,12 @@
+
+
+# 0: Creating a list of students and printing each student's name
+students = ["Ahzam", "Zain", "Ali", "Hassan", "Hussain"]
+
+# Accessing each student's name using their index in the list
+
+print(students[0])  # Output: Ahzam
+print(students[1])  # Output: Zain
+print(students[2])  # Output: Ali
+print(students[3])  # Output: Hassan
+print(students[4])  # Output: Hussain
