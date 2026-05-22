d1bc57f3bf4025a7ccaef18997cd35a8c8fb4db7
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-05-22 13:42:17 +0000

    Add cat04.03.py to implement a while loop for prompting non-negative integer input and a for loop for printing "meow" n times

---
+++ Code_Practices/Loop/cat04.03.py
@@ -0,0 +1,12 @@
+
+# 4: Using a while loop to prompt the user for a non-negative integer until they provide one
+
+while True:
+    n = int(input("What's n?"))
+    if n >= 0:
+        break
+
+# 5: Using a for loop to print "meow" n times
+
+for _ in range(n):
+    print("meow")
