dfa69a3fef4f39962f4cff43fa722df50dfb8c22
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-05-22 14:03:54 +0000

    Add House02.py to create and print a list of students using a for loop

---
+++ Code_Practices/list/House02.py
@@ -0,0 +1,8 @@
+
+students = ["Ahzam", "Zain", "Ali", "Hassan", "Hussain"]
+
+# Printing the entire list of students using for loop
+# using Range function to generate a sequence of numbers from 0 to 4 (5 numbers in total)
+# using len() function to get the length of the list, which is 5 in this case
+for i in range(len(students)):
+    print(i + 1,students[i])
