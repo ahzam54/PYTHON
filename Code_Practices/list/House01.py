5fd1ba3fd75396b33ea1b7885be26ae2f69f696e
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-05-22 13:57:49 +0000

    Add House01.py to create and print a list of students

---
+++ Code_Practices/list/House01.py
@@ -0,0 +1,13 @@
+
+# 1: Creating a list of students and printing the entire list
+
+students = ["Ahzam", "Zain", "Ali", "Hassan", "Hussain"]
+
+#print(students)
+# The above code will print the entire list of students, which is:
+#['Ahzam', 'Zain', 'Ali', 'Hassan', 'Hussain']
+
+
+# Printing the entire list of students using for loop
+for student in students:
+    print(student)
