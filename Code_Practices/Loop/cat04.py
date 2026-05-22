aa19caa4fecc7663ee759eb9fb8772a066106157
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-05-22 13:31:52 +0000

    Add cat04.py to demonstrate string multiplication for printing "meow" three times

---
+++ Code_Practices/Loop/cat04.py
@@ -0,0 +1,12 @@
+
+# 4: Using string multiplication to print "meow" three times
+
+# print("meow" * 3)
+
+# This code prints "meow" three times, with a newline character after each "meow".
+
+#print("meow\n" * 3)
+
+# This code prints "meow" three times without a newline character at the end.
+
+print("meow\n" * 3, end="")  
