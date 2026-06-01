a4a292e5c9938cdecdf50b2451cac91f60224311
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-06-01 01:32:36 +0000

    Add number02.py to demonstrate handling user input with try-except-else for ValueError

--- Code_Practices/Exceptions/number01.py
+++ Code_Practices/Exceptions/number01.py
@@ -1,3 +1,6 @@
+# Prompt the user for a number, and output that number squared.
+#  If the user does not input a number, output an error message.
+
 try:
     x = int(input("What's x? "))
     print(f"x is {x}")
