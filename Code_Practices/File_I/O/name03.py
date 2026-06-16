7db0f7def4ec30b86ae2394c3ef8248a49e9ac56
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-06-16 11:36:57 +0000

    Refactor name input script and enhance file reading functionality

--- Code_Practices/File_I/O/name03.py
+++ Code_Practices/File_I/O/name03.py
@@ -3,4 +3,4 @@ name = input("What's your name? ")
 with open("names.txt", "a") as file:
     file.write(f"{name}\n")
 
-# this code does't need closeing file.
+# this code does't need closeing file.zest
---
+++ Code_Practices/File_I/O/name04.py
@@ -0,0 +1,7 @@
+# read an exting file
+
+with open("names.txt", "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    print("hello,", line.rstrip())
