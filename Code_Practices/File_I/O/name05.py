4419f3fe6cc86b167947c677dcf9ff11a334b394
ahzam54 <102316293+ahzam54@users.noreply.github.com>
2026-06-16 11:42:34 +0000

    Add name sorting script to read and greet from names.txt

---
+++ Code_Practices/File_I/O/name05.py
@@ -0,0 +1,10 @@
+# read the file and sort the text file and output it
+
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
